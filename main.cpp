41812945cc0fbec01e7d40a35767da4ad1a0b14d
Feister, Scott <sfeister@vcccd.edu>
2026-03-10 19:39:59 -0700

    Cleaned up for clarity
    with LLM support

--- main.cpp
+++ main.cpp
@@ -1,43 +1,50 @@
 #include <iostream>
 using namespace std;
 
-const double DOLLARS_PER_GALLON = 12; // how much does a gallon of gas cost?
-const double MILES_PER_COMMUTE = 20.5; // miles of your commute (round trip)
-const double MILES_PER_GALLON = 36; // miles per gallon of your car
+const double DOLLARS_PER_GALLON = 12;     // cost of gas per gallon
+const double MILES_PER_COMMUTE = 20.5;    // miles in one round-trip commute
+const double MILES_PER_GALLON = 36;       // car fuel efficiency
 
-double myTempF;
-double myTempC;
+double myCommutes;
+double myCost;
 
-// Converts degrees Fahrenheit to Celsius
+// Converts gas dollars into number of commutes
 double toCommutes(double gasDollars) {
   double gasGallons;
+  double gasMiles;
   double numCommutes;
-  
+
   gasGallons = gasDollars / DOLLARS_PER_GALLON;
   gasMiles = gasGallons * MILES_PER_GALLON;
-  
-  // TODO! Finish this calculation
-  numCommutes = gasMiles; // not quite right...
-  return numCommutes; // not quite right...
+
+  numCommutes = gasMiles; // TODO: That math is not right! Something is missing here...
+
+  return numCommutes;
 }
 
-// TODO! A function to convert commute minutes into gas cost
-// double toDollars(....) {
-// }
+// TODO!
+// Converts number of commutes into gas cost
+/*
+double toDollars(double commutes) {
+  ....
+  return totalCost;
+}
+*/
 
 int main() {
-  // Convert a variety of commonly-known temperatures into Celsius
+
   cout << "$20 will buy you: " << toCommutes(20) << " commutes." << endl;
   cout << "$100.50 will buy you: " << toCommutes(100.50) << " commutes." << endl;
   cout << "$500 will buy you: " << toCommutes(500) << " commutes." << endl;
   cout << "$8000 will buy you: " << toCommutes(8000) << " commutes." << endl;
 
-  // Prompt user for a number of commutes to convert to cost
   cout << "Enter the number of commutes you'd like to do:" << endl;
   cin >> myCommutes;
+
   myCost = toDollars(myCommutes);
+
   cout << "Great idea! Let's drive! ";
   cout << myCommutes << " commutes will cost you $" << myCost << endl;
-  
+
   cout << "All done!" << endl;
 }
